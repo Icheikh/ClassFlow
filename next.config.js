55b73561e477cc8089f659eee158780e4cdff0e9
Icheikh <cheikhsidi22@gmail.com>
2026-07-04 16:54:21 +0000

    Isolate Next dev output from production build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
-};
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
 
-module.exports = nextConfig;
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    distDir: isDev ? ".next-dev" : ".next",
+    allowedDevOrigins: ["127.0.0.1", "localhost"],
+    images: {
+      domains: ["avatars.githubusercontent.com"],
+    },
+  }
+
+  return nextConfig
+}
